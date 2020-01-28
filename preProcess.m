984f8836886cdc7da9d1b9d4008ba782dc17bce7
Mohi Andrabi <mandrabi@oxy.edu>
2020-01-27 19:26:41 -0800

    Deleted Files

--- preProcess.m
+++
@@ -1,9 +0,0 @@
-labels = longData(:, 8);
-feats = longData(:, 1:7);
-
-[newfeats, newLabels] = removeNAN(feats,labels);
-dataNoNans = newfeats;
-dataNoNans(:, 8) = newLabels;
-
-dataset = balanceData(dataNoNans);
-
