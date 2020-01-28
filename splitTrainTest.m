7514eb195404025bd78172dcbc422c7b8087fabc
Mohi Andrabi <mandrabi@oxy.edu>
2020-01-27 19:27:01 -0800

    Deleted Files

--- splitTrainTest.m
+++
@@ -1,9 +0,0 @@
-function [training, testing] = splitTrainTest(dataSet , P)
-% https://www.mathworks.com/matlabcentral/answers/395136-how-to-divide-a
-%-data-set-randomly-into-training-and-testing-data-set
-[m,~] = size(dataSet);
-idx = randperm(m);
-training = dataSet(idx(1:round(P*m)),:); 
-testing = dataSet(idx(round(P*m)+1:end),:);
-
-end
